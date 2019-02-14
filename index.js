f12ac635eb00bcb65cca922a8b6b3de3aee5d700
Kyle <kylemuch@gmail.com>
2019-02-14 23:25:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(cb, arr) {
-  arr.forEach(el => {
-    cb(el)
-  })
+  arr.forEach(cb)
 }
 
 // Add your changeCompletely() function here:
