3d786a6e0a879eea2101bc1729ee411082ca10bc
Kyle <kylemuch@gmail.com>
2019-02-14 23:29:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(cb, array) {
-  array.forEach(cb);
+  array.forEach(function(el){
+    cb(el)
+  });
 }
 
 // Add your changeCompletely() function here:
